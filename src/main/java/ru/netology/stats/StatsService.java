349fef47806565fb2089d3fdb6750f0fbc3e2320
Usniya <usniya22@mail.ru>
2024-09-15 20:29:29 +0300

    SalesCounter

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,71 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long amountSales(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long averageSales(long[] sales) {
+        //long totalSale = 0;
+        //for (long sale : sales) {
+        //    totalSale += sale;
+        //}
+        //   long average = totalSale / 12;
+        //   return average;
+
+
+        //long totalSale = amountSales(sales);
+        // long average = totalSale / 12;
+        //return average;
+
+        //long average = amountSales(sales) / 12;
+        //return average;
+
+        return amountSales(sales) / sales.length;
+
+    }
+
+    public int maxMonthSales(long[] sales) {
+        int maxMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public int minMonthSales(long[] sales) {
+        int minMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+    public int monthsBellowAverage (long[] sales) {
+        int counter = 0;
+        long average = amountSales(sales);
+        for (long sale : sales) {
+            if (sale < average) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    public int monthsAboveAverage (long[] sales) {
+        int counter = 0;
+        long average = amountSales(sales);
+        for (long sale : sales) {
+            if (sale > average) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,66 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    public void testAmountSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.amountSales(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testAverageSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.averageSales(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public  void  testMaxMonthSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.maxMonthSales(sales);
+        long expected = 8;
+    }
+
+    @Test
+    public  void  testMinMonthSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minMonthSales(sales);
+        long expected = 8;
+    }
+    @Test
+    public  void  testMonthsBellowAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.monthsBellowAverage(sales);
+        long expected = 5;
+    }
+    @Test
+    public  void  testMonthsAboveAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.monthsAboveAverage(sales);
+        long expected = 5;
+    }
+}
